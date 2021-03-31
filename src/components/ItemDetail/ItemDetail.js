78c653824257c98f19cccba7865311304e983e32
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-03-30 22:29:50 -0300

    Se implementa lógica para obtener cantidad seleccionada de ItemCount, y mostrar termino de compra

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,15 +1,41 @@
 import './ItemDetail.css'
-import React from 'react'
+import React , { useState } from 'react'
+import { Link } from 'react-router-dom'
 import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({ item }) => {
+    const [count, setCount ] = useState(0);
+    const [showCartButton, setShowCartButton ] = useState(false);
+
+    const addQtyItems = (qty) => {
+        setCount(qty)
+        setShowCartButton(qty > 0) 
+    }
+
     return (
         <div className="container_item-detail row">
             <div className="col-md-7">
-                <h1 className="display-4">{item?.title}</h1>
-                <p className="lead">{item?.description}</p>
-                <h1 className="display-5">$ {item?.price}</h1>
-                <ItemCount initial={1} stock={item?.stock}/>
+                <div className="row">
+                    <div className="col-md">
+                        <h1 className="display-4">{item?.title}</h1>
+                        <p className="lead">{item?.description}</p>
+                        <h1 className="display-5">$ {item?.price}</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md">
+                        {
+                            showCartButton ? 
+                                <div>
+                                    <p>Comprarás {count} item(s)</p>
+                                    <Link className="btn btn-info" to="/cart">
+                                        Terminar mi compra
+                                    </Link>
+                                </div> :
+                                <ItemCount initial={1} stock={item?.stock} onAdd={addQtyItems}/>
+                        }
+                    </div>
+                </div>
             </div>
             <div className="col-md-5">
                 <img src={item?.imgUrl} className="img-fluid img-thumbnail" alt={item?.title} />

ea12a3e64216d7fd34bc199ce9fde9a476af507e
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-04-06 22:11:42 -0400

    Se integra componente CartContext a la app

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import NavBar from './containers/NavBar/NavBar'
 import ItemListContainer from './containers/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './containers/ItemDetailContainer/ItemDetailContainer'
 import CartContainer from './containers/CartContainer/CartContainer'
+import { CartContext } from './contexts/cartContext'
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={ItemListContainer} />
-          <Route path="/category/:categoryId" component={ItemListContainer} />
-          <Route path="/item/:itemId" component={ItemDetailContainer}/>
-          <Route path="/cart" component={CartContainer} />
-          <Route exact path="*">
-            Generar un 404 page
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <CartContext>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={ItemListContainer} />
+            <Route path="/category/:categoryId" component={ItemListContainer} />
+            <Route path="/item/:itemId" component={ItemDetailContainer}/>
+            <Route path="/cart" component={CartContainer} />
+            <Route exact path="*">
+              Generar un 404 page
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </CartContext>
   );
 }
 

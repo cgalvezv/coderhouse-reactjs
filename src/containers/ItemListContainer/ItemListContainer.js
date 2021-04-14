e8600371608ed998bae6c9c81ca3c732062a4427
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-04-13 22:24:06 -0400

    Se integra lógica para acceder a los items a través de firebase

--- src/containers/ItemListContainer/ItemListContainer.js
+++ src/containers/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,6 @@
 import React, { useState , useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { getItems } from '../../db/mockData'
+import { getItems } from '../../db/firebase'
 import ItemList  from '../../components/ItemList/ItemList'
 import { LoadingPage } from '../../utils'
 import './ItemListContainer.css'
@@ -8,17 +8,16 @@ import './ItemListContainer.css'
 const ItemListContainer = () => {
     const { categoryId } = useParams()
     const [items, setItems] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
     
     useEffect(() => {
-        getItems().then((items) => {
-            const itemsByCategory = categoryId !== undefined ? items.filter(item => item.categoryId === Number(categoryId)) : items
-            setItems(itemsByCategory)
-            setLoading(false)
-        })
-        return () => {
-            setLoading(true)
-        };
+        setLoading(true);
+        let items = categoryId !== undefined ? getItems().where('categoryId', '==', Number(categoryId)) : getItems();
+        items.get().then((querySnapshot) => {
+            if (querySnapshot.size === 0) console.log('no results!!!');
+            setItems(querySnapshot.docs.map(doc => { return { id: doc.id, ...doc.data() } }));
+        }).catch((err) => console.log(`Error finding items ${JSON.stringify(err, null, 2)}`))
+        .finally(() => setLoading(false))
     }, [categoryId])
 
     return (

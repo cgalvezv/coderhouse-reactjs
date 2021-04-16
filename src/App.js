1a0e806812fd3d878fda06898b9e930b1933449d
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-04-15 23:09:03 -0400

    Se agrega ruta apuntando al final del proceso del carro

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './containers/NavBar/NavBar'
 import ItemListContainer from './containers/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './containers/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
+import ResultPage from './pages/ResultPage/ResultPage'
 import { CartContext } from './contexts/cartContext'
 import './App.css';
 
@@ -16,7 +17,15 @@ function App() {
             <Route exact path="/" component={ItemListContainer} />
             <Route path="/category/:categoryId" component={ItemListContainer} />
             <Route path="/item/:itemId" component={ItemDetailContainer}/>
-            <Route path="/cart" component={Cart} />
+            <Route exact path="/cart" component={Cart} />
+            <Route path="/cart/finished" component={
+              () => <ResultPage 
+                titleText="Compra finalizada exitosamente"
+                buttonText="Volver al Home"
+                redirectPath="/"
+                useSuccessfullyIcon={true}
+              />
+            } />
             <Route exact path="*">
               Generar un 404 page
             </Route>

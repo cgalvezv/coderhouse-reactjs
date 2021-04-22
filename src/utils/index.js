2cc4b1f30483a80fa23a13daa7a54caf03cffaf4
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-04-22 16:39:11 -0400

    Se migra código componente ResultPage a utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
-import './index.css'
 import React from 'react'
+import { useParams, Link } from 'react-router-dom';
+import logoSuccesfully from '../assets/img/tick.png'
+import './index.css'
 
 export const LoadingPage = () => {
     return (
@@ -10,4 +12,27 @@ export const LoadingPage = () => {
         </div>
     )
 }
+
+export const ResultPage = ({ titleText, buttonText, redirectPath, useSuccessfullyIcon = false}) => {
+    const { orderId } = useParams()
+    
+    const hasOrderID = () => orderId !== undefined;
+
+    return (
+        <div className="container text-center">
+            <div className="content-wrappper_result-page">
+                { useSuccessfullyIcon && <img className="img-responsive logo-size_result-page" src={logoSuccesfully} alt="Successfully Icon" /> }
+                <h1 className="display-4">{titleText}</h1>
+                {
+                    hasOrderID() && <p className="lead">
+                        El código identificador de la orden generada para esta compra es <b>{orderId}</b>. <br/> Gracias por preferirnos
+                    </p>
+                }
+                <br></br>
+                <Link className="btn btn-primary" to={redirectPath}>{buttonText}</Link>
+            </div>
+        </div>
+    )
+}
+
 
